95a7ea8a72aa286fe9b01c4e8b666d3ccf0910c2
Arthur <arthur.albuquerque.1995@gmail.com>
2020-11-04 13:32:05 -0300

    + lista que armazena todos os valores de x

--- Main.py
+++ Main.py
@@ -4,12 +4,13 @@
 from random import randint
 
 def simula_questao_1(numero_de_simulacoes):
+    lista_dos_x = []
+    media = float(0)
     #loop para simular a quantidade de vezes desejada
     for i in range(numero_de_simulacoes):
         #enquanto nao sortear um numero igual
         #x vai ser o numero de repeticoes ate ser sorteada uma bola 2 vezes
         x = 0
-        media = float(0)
         #vetor que vai armazenar as bolas que foram sorteadas
         bolas_sorteadas = [False] * 12
         for j in range(11):
@@ -20,9 +21,12 @@ def simula_questao_1(numero_de_simulacoes):
                 media = media + x
                 print(str(bolas_sorteadas))
                 print("x: ", x)
+                lista_dos_x.append(x)
                 break
             bolas_sorteadas[bola_sorteada] = True
     media = media/numero_de_simulacoes
+    print(str(lista_dos_x))
     print('media: ', media)
+
 
 simula_questao_1(10)
